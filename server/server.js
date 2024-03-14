fc793e8c77c5377eb091dd909f00bf9253d2fd85
pavan2580-dmpt <pavanganesh2580@gmail.com>
2024-03-14 15:03:26 +0530

    updated .env

--- server/server.js
+++ server/server.js
@@ -17,4 +17,4 @@ app.use("/", require("./Routes/routers"));
 
 DataBase();
 
-app.listen(3000, () => console.log("Server is running on port 3000"));
+app.listen(process.env.PORT , () => console.log(`Server is running on port ${process.env.PORT}`));
