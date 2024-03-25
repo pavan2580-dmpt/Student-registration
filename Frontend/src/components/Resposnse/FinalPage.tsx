9bf4a9e4b115f06ffd98131053183d02c264fa0b
pavan2580-dmpt <pavanganesh2580@gmail.com>
2024-03-25 13:39:15 +0530

    added button in finalpage

--- Frontend/src/components/Resposnse/FinalPage.tsx
+++ Frontend/src/components/Resposnse/FinalPage.tsx
@@ -1,21 +1,25 @@
 import { useEffect } from "react";
-import { useLocation,useNavigate } from "react-router-dom"
+import { Link, useLocation, useNavigate } from "react-router-dom";
 function FinalPage() {
   const location = useLocation();
-  const nav = useNavigate()
-  const val = location.state?.show
+  const nav = useNavigate();
+  const val = location.state?.show;
   useEffect(() => {
     if (val === undefined) {
       nav("/");
     }
-  },[]);
+  }, []);
 
   return (
     <div className="w-f h-[100vh] flex justify-center items-center">
-      <h1 className="text-2xl"><p className=" text-red-500">{val?.name }</p> your Resposne is submited</h1>
-
+      <h1 className="text-2xl">
+        <p className=" text-red-500">{val?.name}</p> your Resposne is submited
+      </h1>
+      <Link to={"/"}>
+        <button>Back to Home</button>
+      </Link>
     </div>
-  )
+  );
 }
 
-export default FinalPage
+export default FinalPage;
