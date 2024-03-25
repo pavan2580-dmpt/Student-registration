7419e905810de6f6ff051ef091b9b4f423a1dff7
pavan2580-dmpt <pavanganesh2580@gmail.com>
2024-03-25 13:25:09 +0530

    changed server.js to index.js

--- server/Routes/routers.js
+++ server/Routes/routers.js
@@ -1,88 +1,93 @@
-const express = require('express')
+const express = require("express");
 const router = express.Router();
-const STUDENT = require("../Models/Students")
+const STUDENT = require("../Models/Students");
 
 // @check student register number,get----------
-router.route("/getUserInfo/:Reg").get(
-    async(req,res)=>{
-        const Reg = req.params.Reg;
-        const Exists = await STUDENT.findOne({registerNo:Reg})
-        if (Exists){
-            res.send(Exists)
-        }
-        else{
-            res.send("new user")
-        }
-
-    }
-)
-// @post 
+router.route("/getUserInfo/:Reg").get(async (req, res) => {
+  const Reg = req.params.Reg;
+  const Exists = await STUDENT.findOne({ registerNo: Reg });
+  if (Exists) {
+    res.send(Exists);
+  } else {
+    res.send("new user");
+  }
+});
+// @post
 // addeds the student to the database
-router.route("/AddStudentInfo").post(
-    async(req,res)=>{
-       try{
-        const {fullName,registerNo,phoneNumber,email,fatherName,motherName} = req.body;
-        if(!fullName || !registerNo || !phoneNumber || !email || !fatherName ||!motherName){
-            res.status(400).send("missing data")
-        }
-        else{
-            const Exists =await STUDENT.findOne({registerNo})
-            if(Exists){
-                res.send(Exists)
-            }
-            else{
-                const Response = await STUDENT.insertMany({
-                    name:fullName,
-                    registerNo:registerNo,
-                    phoneNo:phoneNumber,
-                    email:email,
-                    fatherName:fatherName,
-                    motherName:motherName
-                })
-                res.send(Response)
-            }
-        }
-       }
-       catch (err){
-        console.log(err)
-        res.status(500).send("Internal server error")
+router.route("/AddStudentInfo").post(async (req, res) => {
+  try {
+    const { fullName, registerNo, phoneNumber, email, fatherName, motherName } =
+      req.body;
+    if (
+      !fullName ||
+      !registerNo ||
+      !phoneNumber ||
+      !email ||
+      !fatherName ||
+      !motherName
+    ) {
+      res.status(400).send("missing data");
+    } else {
+      const Exists = await STUDENT.findOne({ registerNo });
+      if (Exists) {
+        res.send(Exists);
+      } else {
+        const Response = await STUDENT.insertMany({
+          name: fullName,
+          registerNo: registerNo,
+          phoneNo: phoneNumber,
+          email: email,
+          fatherName: fatherName,
+          motherName: motherName,
+        });
+        res.send(Response);
       }
     }
-)
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Internal server error");
+  }
+});
 
 // update
 // update the student data
-router.route("/updateTheInfo").put(
-    async(req,res)=>{
-        try {
-            const{email,fatherName,fullName,motherName,phoneNumber,registerNo} = req.body
-            if(!fullName || !registerNo || !phoneNumber || !email || !fatherName ||!motherName){
-                res.status(400).send(req.body)
-            }
-            else{
-                const Exists = await STUDENT.findOne({registerNo})
-                if(!Exists){
-                    res.send("no user found")
-                }
-                else{
-                    const update = await STUDENT.findOneAndUpdate({registerNo:registerNo},{
-                        $set:{
-                            email:email,
-                            name:fullName,
-                            phoneNo:phoneNumber,
-                            fatherName:fatherName,
-                            motherName:motherName
-                        }
-                    },{new:true});
-                    res.send(update)
-                }               
-            }
-        } catch (error) {
-            res.status(500).send("internal error")
-            
-        }
-    
+router.route("/updateTheInfo").put(async (req, res) => {
+  try {
+    const { email, fatherName, fullName, motherName, phoneNumber, registerNo } =
+      req.body;
+    if (
+      !fullName ||
+      !registerNo ||
+      !phoneNumber ||
+      !email ||
+      !fatherName ||
+      !motherName
+    ) {
+      res.status(400).send(req.body);
+    } else {
+      const Exists = await STUDENT.findOne({ registerNo });
+      if (!Exists) {
+        res.send("no user found");
+      } else {
+        const update = await STUDENT.findOneAndUpdate(
+          { registerNo: registerNo },
+          {
+            $set: {
+              email: email,
+              name: fullName,
+              phoneNo: phoneNumber,
+              fatherName: fatherName,
+              motherName: motherName,
+            },
+          },
+          { new: true }
+        );
+        res.send(update);
+      }
     }
-)
+  } catch (error) {
+    res.status(500).send("internal error");
+  }
+});
 
-module.exports = router
+module.exports = router;
