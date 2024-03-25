15f96501ef9255d41be654ad3a53006b318555b9
pavan2580-dmpt <pavanganesh2580@gmail.com>
2024-03-25 13:42:54 +0530

    update the cors origin

--- server/index.js
+++ server/index.js
@@ -5,16 +5,25 @@ const DataBase = require("./DataBase/DB");
 const bodyParser = require("body-parser");
 require("dotenv").config();
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 app.use(express.json());
-app.use(cors("*"));
+app.use(
+  cors({
+    origin:
+      "https://collage-test-project-6chv-g6un64p6c-pavan2580-dmpts-projects.vercel.app",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 app.get("/", (req, res) => {
-    res.send("From the server");
+  res.send("From the server");
 });
 
 app.use("/", require("./Routes/routers"));
 
 DataBase();
 
-app.listen(process.env.PORT , () => console.log(`Server is running on port ${process.env.PORT}`));
+app.listen(process.env.PORT, () =>
+  console.log(`Server is running on port ${process.env.PORT}`)
+);
