b7915a84a7619c1fc1fbd165d54f09904df6079e
pavan2580-dmpt <pavanganesh2580@gmail.com>
2024-05-30 16:36:45 +0530

    updated url

--- server/index.js
+++ server/index.js
@@ -21,7 +21,6 @@ app.get("/", (req, res) => {
 
 app.use("/", require("./Routes/routers"));
 
-
 app.listen(process.env.PORT, () =>
   console.log(`Server is running on port ${process.env.PORT}`)
 );
